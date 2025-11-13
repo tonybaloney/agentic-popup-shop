de97204eaf0eea2f84e5b195bc376a4b40519e89
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-11-13 03:22:05 +0000

    Update to release packages

--- apphost.cs
+++ apphost.cs
@@ -40,7 +40,7 @@ var supplierMcp = builder.AddPythonModule("supplier-mcp", "./app/mcp/", "zava_sh
 
 var agentDev = builder.AddPythonModule("agent-dev", "./app/agents/", "zava_shop_agents")
     // TODO : Remove index strategy once the new SDK is published to PyPi
-    .WithUv(args: ["sync", "--index-strategy", "unsafe-best-match", "--link-mode=copy"])
+    .WithUv(args: ["sync", "--prerelease=allow", "--link-mode=copy"])
     .WithHttpEndpoint(env: "PORT")
     .WithHttpHealthCheck("/health")
     .WithEnvironment("OTEL_PYTHON_EXCLUDED_URLS", "/health")
@@ -64,7 +64,7 @@ var agentDev = builder.AddPythonModule("agent-dev", "./app/agents/", "zava_shop_
 var apiService = builder.AddPythonModule("api", "./app/api/", "uvicorn")
     .WithArgs("zava_shop_api.app:app", "--reload")
     // TODO : Remove index strategy once the new SDK is published to PyPi
-    .WithUv(args: ["sync", "--index-strategy", "unsafe-best-match", "--link-mode=copy"])
+    .WithUv(args: ["sync", "--prerelease=allow", "--link-mode=copy"])
     .WithCertificateTrustScope(CertificateTrustScope.System)
     .WithHttpEndpoint(env: "UVICORN_PORT")
     .WithHttpHealthCheck("/health")
